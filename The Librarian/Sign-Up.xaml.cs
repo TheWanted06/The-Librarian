858e016d989bb39dc44d5300c091c7d009a61520
19331512 <19331512@VCDWLIBPC12>
2021-09-27 10:17:04 +0200

    Add project files.

---
+++ The Librarian/Sign-Up.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace The_Librarian
+{
+    /// <summary>
+    /// Interaction logic for Sign_Up.xaml
+    /// </summary>
+    public partial class Sign_Up : Window
+    {
+        public Sign_Up()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ The Librarian/Splash-Screen.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace The_Librarian
+{
+    /// <summary>
+    /// Interaction logic for Splash_Screen.xaml
+    /// </summary>
+    public partial class Splash_Screen : Window
+    {
+        public Splash_Screen()
+        {
+            InitializeComponent();
+        }
+    }
+}
